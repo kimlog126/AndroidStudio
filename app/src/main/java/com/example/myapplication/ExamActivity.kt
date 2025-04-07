8aaf5b7d38850b76f128df9807bf36f1d53598cc
kimlog126 <jisu11260@gmail.com>
2025-04-07 15:05:49 +0900

    연습문제 5 - RecyclerView 실습

--- app/src/main/java/com/example/myapplication/ExamActivity.kt
+++ app/src/main/java/com/example/myapplication/ExamActivity.kt
@@ -37,7 +37,6 @@ class ExamActivity : AppCompatActivity() {
 
 
     private fun prepareExamData() {
-        // 이미지의 예시 데이터 추가
         examList.add(Exam("First Exam", "2025.03.15", "최선을 다하자!!"))
         examList.add(Exam("Midterm Exam", "2025.05.10", "재수강 할까?"))
         examList.add(Exam("Final Exam", "2025.06.15", "내가 제일 마지막에 끝낼듯..."))
